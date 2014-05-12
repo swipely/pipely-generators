20e37d65d0ed68d726ef8255bea64ca7a942f1a6
Matt Gillooly <matt@swipely.com>
2014-05-12 19:01:40 -0400

    remove Swipely-specific bootstrap code

--- templates/steps/bootstrap_emr.sh
+++ templates/steps/bootstrap_emr.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sudo apt-get -y install rubygems
-sudo gem install streamingly --no-ri --no-rdoc --source http://rubygems.org
 
-# TODO: install any gems that will be needed by streaming EMR activities
+set -e
 
-exit
+# TODO: Install whatever your EmrCluster will need.
+
+# sudo apt-get -y install rubygems
+# sudo gem install streamingly --no-ri --no-rdoc --source http://rubygems.org
